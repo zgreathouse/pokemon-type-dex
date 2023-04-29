411d20e3496b35d89db3f30841e9a8bf8e2ad972
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-04-29 16:24:32 -0700

    generate skeletons for home, pokemon, and moves components

---
+++ src/app/features/moves/moves.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-moves',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './moves.component.html',
+  styleUrls: ['./moves.component.scss']
+})
+export class MovesComponent {
+
+}
