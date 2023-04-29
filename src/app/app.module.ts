04888602c69d7d1d2c935f4dd252eb87c988bf0c
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-04-29 15:05:50 -0700

    generates skeleton nav component and imports it in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [AppRoutingModule, BrowserModule, NavComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
