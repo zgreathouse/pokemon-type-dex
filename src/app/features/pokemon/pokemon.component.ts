4a34327d46de9b5b6b98f8d54ab7206322c93811
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-06-18 15:30:35 -0700

    wires up PokemonComponent with PokemonService

--- src/app/features/pokemon/pokemon.component.ts
+++ src/app/features/pokemon/pokemon.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PokemonService } from './pokemon.service';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
   imports: [CommonModule],
+  providers: [PokemonService],
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.scss'],
 })
-export class PokemonComponent {}
+export class PokemonComponent {
+  pokemon$ = this.pokemonService.filteredPokemon$.pipe();
+
+  constructor(private pokemonService: PokemonService) {}
+}
