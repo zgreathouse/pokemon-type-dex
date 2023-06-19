e49f53d42dd6397e2d2dfb8e1692d5f75d371c73
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-06-18 20:36:26 -0700

    creates skeleton for pokemon-card

--- src/app/features/pokemon/pokemon.component.ts
+++ src/app/features/pokemon/pokemon.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonService } from './pokemon.service';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PokemonCardComponent],
   providers: [PokemonService],
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.scss'],
