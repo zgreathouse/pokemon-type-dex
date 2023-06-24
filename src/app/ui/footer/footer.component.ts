4f19bf21ed74947b073b2f126c74c4b26d643a33
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-06-24 11:36:20 -0700

    updates footer to utilize angular-material

--- src/app/ui/footer/footer.component.ts
+++ src/app/ui/footer/footer.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatToolbarModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  links = [
+    {
+      label: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/zach-greathouse/',
+    },
+    {
+      label: 'Github',
+      href: 'https://github.com/zgreathouse/pokemon-type-dex',
+    },
+  ];
+}
