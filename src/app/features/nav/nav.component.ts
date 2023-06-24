e56de62183aa4d045af57930bc313f60f29b92bb
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-06-24 11:24:27 -0700

    updates top-nav to utilize angular material

--- src/app/features/nav/nav.component.ts
+++ src/app/features/nav/nav.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatToolbarModule],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
