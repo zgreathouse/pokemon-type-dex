0ae89c7b5cc2477c740641b312b416827faf9181
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-06-24 12:22:00 -0700

    adds pokemon search auto-complete to pokeon component

--- src/app/features/pokemon/pokemon.component.ts
+++ src/app/features/pokemon/pokemon.component.ts
@@ -1,18 +1,48 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { PokemonService } from './pokemon.service';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { debounceTime, distinctUntilChanged, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [CommonModule, PokemonCardComponent],
+  imports: [
+    CommonModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    PokemonCardComponent,
+    ReactiveFormsModule,
+  ],
   providers: [PokemonService],
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.scss'],
 })
 export class PokemonComponent {
-  pokemon$ = this.pokemonService.filteredPokemon$.pipe();
+  myControl = new FormControl('');
+
+  readonly POKEMON_NAMES = this.pokemonService
+    .fetchPokemon()
+    .map((pokemon) => pokemon.name.toLocaleLowerCase());
+
+  filteredPokemon$ = this.myControl.valueChanges.pipe(
+    startWith(''),
+    distinctUntilChanged(),
+    debounceTime(300),
+    map((value) => this.filterPokemon(value || ''))
+  );
 
   constructor(private pokemonService: PokemonService) {}
+
+  private filterPokemon(value: string): string[] {
+    const formattedValue = value.toLowerCase();
+    return this.POKEMON_NAMES.filter((pokemonName) =>
+      pokemonName.includes(formattedValue)
+    );
+  }
 }
