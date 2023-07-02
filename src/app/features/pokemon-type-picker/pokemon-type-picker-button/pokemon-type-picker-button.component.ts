4c939c81245f4a9d509934ca65d9eeb5bc8bde0c
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-07-02 10:08:55 -0700

    updates selected color of pokemon type picker buttons to match pokemon type color

--- src/app/features/pokemon-type-picker/pokemon-type-picker-button/pokemon-type-picker-button.component.ts
+++ src/app/features/pokemon-type-picker/pokemon-type-picker-button/pokemon-type-picker-button.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, Input, Output, Type } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PokemonType } from '@types';
+import { PokemonType, PokemonTypeColor } from '@types';
 import { MatButtonModule } from '@angular/material/button';
 import { PokemonTypePickerService } from '../pokemon-type-picker.service';
 import { map, Observable } from 'rxjs';
+import { pokemonTypeColors } from 'src/app/ui/pokemon-type-chip/pokemon-type-chip';
 
 @Component({
   selector: 'app-pokemon-type-picker-button',
@@ -12,16 +13,21 @@ import { map, Observable } from 'rxjs';
   templateUrl: './pokemon-type-picker-button.component.html',
   styleUrls: ['./pokemon-type-picker-button.component.scss'],
 })
-export class PokemonTypePickerButtonComponent {
+export class PokemonTypePickerButtonComponent implements OnInit {
   @Input() pokemonType!: PokemonType;
-  @Output() typeSelectionEvent = new EventEmitter<PokemonType>();
 
-  readonly selected$: Observable<boolean> =
+  selected$: Observable<boolean> =
     this.pokemonTypePickerService.selectedPokemonType$.pipe(
       map((selectedType) => selectedType === this.pokemonType)
     );
 
+  pokemonTypeColor!: PokemonTypeColor;
+
   constructor(private pokemonTypePickerService: PokemonTypePickerService) {}
+
+  ngOnInit(): void {
+    this.pokemonTypeColor = pokemonTypeColors[this.pokemonType];
+  }
 
   selectType(): void {
     this.pokemonTypePickerService.updateSelectedType(this.pokemonType);
