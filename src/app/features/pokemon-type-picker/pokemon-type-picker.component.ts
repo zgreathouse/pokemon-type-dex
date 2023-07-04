cfd32da520b90a449e867477cec16dba5adad695
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-07-04 15:21:56 -0700

    consolidates pokemon-type-picker componentry

--- src/app/features/pokemon-type-picker/pokemon-type-picker.component.ts
+++ src/app/features/pokemon-type-picker/pokemon-type-picker.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PokemonTypePickerButtonComponent } from './pokemon-type-picker-button/pokemon-type-picker-button.component';
 import { POKEMON_TYPES } from './pokemon-types';
 import { PokemonService } from 'src/app/services/pokemon.service';
+import { PokemonType } from '@types';
+import { pokemonTypeColors } from 'src/app/ui/pokemon-type-chip/pokemon-type-chip';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-pokemon-type-picker',
   standalone: true,
-  imports: [CommonModule, PokemonTypePickerButtonComponent],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './pokemon-type-picker.component.html',
   styleUrls: ['./pokemon-type-picker.component.scss'],
 })
 export class PokemonTypePickerComponent {
   readonly pokemonTypes = POKEMON_TYPES;
+  readonly pokemonTypeColors = pokemonTypeColors;
+
   readonly selectedType$ = this.pokemonService.selectedPokemonType$;
 
   constructor(private pokemonService: PokemonService) {}
+
+  selectType(pokemonType: PokemonType): void {
+    this.pokemonService.updateSelectedType(pokemonType);
+  }
 }
