9328d0431328555a8a511bc505370a2a2158445f
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-07-05 11:25:00 -0700

    sets up route redirection to /home when visiting an invlaid route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
-  {
     path: 'home',
     loadComponent: () =>
       import('./ui/frame/frame.component').then((m) => m.FrameComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full',
   },
 ];
