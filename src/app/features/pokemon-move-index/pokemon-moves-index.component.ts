46720fac51e031533833b3b875ece7fe6319ed0f
Zach Greathouse <zach@Zachs-MacBook-Air.local>
2023-07-06 13:12:25 -0700

    adds sorting to pokemon moves index table

--- src/app/features/pokemon-move-index/pokemon-moves-index.component.ts
+++ src/app/features/pokemon-move-index/pokemon-moves-index.component.ts
@@ -1,28 +1,86 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonTypeChipComponent } from 'src/app/features/pokemon-type-chip/pokemon-type-chip.component';
-import { MatTableModule } from '@angular/material/table';
+import {
+  MatTableDataSource,
+  MatTableDataSourcePaginator,
+  MatTableModule,
+} from '@angular/material/table';
 import { PokemonService } from 'src/app/services/pokemon.service';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { PokemonMoveDetail } from '@types';
+import { takeUntil, tap } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-moves-index',
   standalone: true,
-  imports: [CommonModule, MatTableModule, PokemonTypeChipComponent],
+  imports: [
+    CommonModule,
+    MatSortModule,
+    MatTableModule,
+    PokemonTypeChipComponent,
+  ],
   templateUrl: './pokemon-moves-index.component.html',
   styleUrls: ['./pokemon-moves-index.component.scss'],
 })
-export class PokemonMovesIndexComponent {
-  displayedColumns = [
-    'Name',
-    'Type',
-    'Category',
-    'Power',
-    'Accuracy',
-    'PP',
-    'Effect',
-    '%',
+export class PokemonMovesIndexComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+
+  readonly displayedColumns = [
+    'pokemonType',
+    'name',
+    'category',
+    'power',
+    'accuracy',
+    'pp',
+    'effect',
+    'probability',
   ];
-  filteredPokemonMoves$ = this.pokemonService.movesOfSelectedType$;
+  readonly nameColumnHeader = 'Name';
+  readonly typeColumnHeader = 'Type';
+  readonly categoryColumnHeader = 'Category';
+  readonly powerColumnHeader = 'Power';
+  readonly accuracyColumnHeader = 'Accuracy';
+  readonly ppColumnHeader = 'PP';
+  readonly effectColumnHeader = 'Effect';
+  readonly probabilityColumnHeader = '%';
+
+  dataSource!: MatTableDataSource<
+    PokemonMoveDetail,
+    MatTableDataSourcePaginator
+  >;
+
+  private destroy$ = new Subject<boolean>();
 
   constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.pokemonService.movesOfSelectedType$
+      .pipe(
+        tap((moves) => {
+          this.dataSource = new MatTableDataSource(moves);
+          if (this.sort) this.dataSource.sort = this.sort;
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
 }
